cffdc666f06a274f2355b742fe2a6b65163127c4
Bogdan <kushnir.2001.bogdan@gmail.com>
2021-06-16 12:28:39 +0300

    client init

--- models/index.ts
+++ models/index.ts
@@ -1,8 +1,5 @@
 import { DeepPartial, FindConditions, getRepository, ObjectType, SaveOptions } from "typeorm";
 import { getClassName } from "../utils";
-import { Group } from "./Group";
-import { Permission } from "./Permission";
-import { User } from "./User";
 
 export abstract class MyBaseEntity {
 
@@ -20,42 +17,6 @@ export abstract class MyBaseEntity {
      */
     static getModelName<T>(model: ObjectType<T> = this): string {
         return getClassName(model).toLowerCase();
-    }
-
-    /**
-     * Предоставляет название права доступа на создание записи.
-     * @param model Екземпляр или тип модели.
-     * @returns Право доступа на создание записи.
-     */
-    public static getCreatePermission<T>(model: ObjectType<T> = this): string {
-        return `can_create_${MyBaseEntity.getModelName(model)}`;
-    }
-
-    /**
-     * Предоставляет название права доступа на редактирование записи.
-     * @param model Екземпляр или тип модели.
-     * @returns Право доступа на редактирование записи.
-     */
-    public static getEditPermission<T>(model: ObjectType<T> = this): string {
-        return `can_edit_${MyBaseEntity.getModelName(model)}`;
-    }
-
-    /**
-     * Предоставляет название права доступа на чтение записи.
-     * @param model Екземпляр или тип модели.
-     * @returns Право доступа на чтение записи.
-     */
-    public static getReadPermission<T>(model: ObjectType<T> = this): string {
-        return `can_read_${MyBaseEntity.getModelName(model)}`;
-    }
-
-    /**
-     * Предоставляет название права доступа на удаление записи.
-     * @param model Екземпляр или тип модели.
-     * @returns Право доступа на удаление записи.
-     */
-    public static getDeletePermission<T>(model: ObjectType<T> = this): string {
-        return `can_delete_${MyBaseEntity.getModelName(model)}`;
     }
 }
 
@@ -75,12 +36,3 @@ export async function findOrCreate<T>(model: ObjectType<T>, selector: FindCondit
 
     return instance;
 }
-
-/**
- * Список всех моделей в порядке инициализации.
- */
-export const models: MyBaseEntity[] = [
-    Permission,
-    Group,
-    User,
-];
