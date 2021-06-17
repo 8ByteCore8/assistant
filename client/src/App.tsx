000c738caf1c53e4ff557b2fd3ab277c072900ce
justrelaxdc <justrelaxdc@gmail.com>
2021-06-17 17:30:52 +0300

    Доработки фронта

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import LoginPage from "./pages/login/Login";
+// import {useDispatch, useSelector} from "react-redux";
+// import {setCount} from "./reducers/reposReducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // const [status, setStatus] = useState(false);
+    //
+    // useEffect(() => {
+    //     console.log("123");
+    // }, [])
+    // const dispatch = useDispatch();
+    // const count = useSelector((state: any) => state.repos.count);
+
+    return (
+        <Router>
+            {/*{count}*/}
+            {/*<button onClick={() => dispatch(setCount(count + 1))}>TEXT</button>*/}
+            <Route path='/login' component={LoginPage}/>
+        </Router>
+    );
+};
 
 export default App;
