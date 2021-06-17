15389e5ea6035d79ba2207002c2fe9a16486d00e
Bogdan <kushnir.2001.bogdan@gmail.com>
2021-06-17 21:24:10 +0300

    немного переделал

--- config.ts
+++ config.ts
@@ -90,8 +90,6 @@ const config: AppConfig = {
             migrationsDir: join(__dirname, "./migrations"),
             subscribersDir: join(__dirname, "./subscribers"),
         },
-        // synchronize: DEBUG,
-        // dropSchema: DEBUG,
     }
 };
 
